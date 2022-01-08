e8727b9e2f15986f0127dc421d22b40f9a8de460
nastyaImp <imperiian@yandex.ru>
2022-01-08 15:55:21 +0300

    testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,32 @@
+package ru.netology.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @org.testng.annotations.Test
+    public void shouldCalculateSumLessThan1000() {
+        int amount = 900;
+        int expected = 100;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void shouldCalculateSumEqualThan1000() {
+        int amount = 1000;
+        int expected = 1000;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void shouldCalculateSumMoreThan1000() {
+        int amount = 1100;
+        int expected = 900;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+
+}
